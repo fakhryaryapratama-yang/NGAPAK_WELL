a174b6f8ea4c754c177983512b9b4647ba19a026
danendra <dzaki.danen@gmail.com>
2026-02-19 09:14:57 +0700

    rename

---
+++ saldo.py
@@ -0,0 +1,11 @@
+def hitung_saldo():
+    conn = get_connection()
+    cur = conn.cursor()
+    cur.execute("""
+        SELECT
+        SUM(CASE WHEN jenis='masuk' THEN jumlah ELSE -jumlah END)
+        FROM kas
+    """)
+    saldo = cur.fetchone()[0]
+    conn.close()
+    return saldo if saldo else 0
