d59ef0f6e81137965696b0bfc6f1cdf952b9c831
danendra <dzaki.danen@gmail.com>
2026-02-19 10:41:04 +0700

    menambahkan penghitungan saldo

--- saldo.py
+++ saldo.py
@@ -1,3 +1,7 @@
+import os
+import sqlite3
+from datetime import datetime
+
 def hitung_saldo():
     conn = get_connection()
     cur = conn.cursor()
@@ -9,3 +13,28 @@ def hitung_saldo():
     saldo = cur.fetchone()[0]
     conn.close()
     return saldo if saldo else 0
+
+def input_kas(jenis):
+    conn = get_connection()
+    cur = conn.cursor()
+
+    petugas = get_petugas()
+    keterangan = input("Keterangan      : ")
+    jumlah = input_rupiah("Jumlah (Rp)     : ")
+
+    cur.execute("""
+        INSERT INTO kas (tanggal, petugas, jenis, keterangan, jumlah)
+        VALUES (?, ?, ?, ?, ?)
+    """, (
+        datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        petugas,
+        jenis,
+        keterangan,
+        jumlah
+    ))
+
+    conn.commit()
+    conn.close()
+
+    print("\n✅ Transaksi berhasil dicatat")
+    print(f"Saldo saat ini : {format_rupiah(hitung_saldo())}")
