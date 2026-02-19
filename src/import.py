f2a227a7ed498b6fa59a26f955ebd31f57c1a7f0
faryap <faryap1511@gmail.com>
2026-02-19 08:46:25 +0700

    format & input rupiah

--- src/import.py
+++ src/import.py
@@ -6,3 +6,19 @@ from reportlab.pdfgen import canvas
 
 DB_FILE = "kas_takmir.db"
 PDF_FILE = "Laporan_Kas_Takmir.pdf"
+
+#==========================
+# Format & Input Rupiah
+#==========================
+def format_rupiah(angka):
+    return f"Rp {angka:,}".replace(","",")
+
+def input_rupiah(prompt):
+    while True:
+        try:
+            nilai = input(prompt)
+            nilai_bersih = nilai.replace(".", "").replace(",", "")
+            return int(nilai_bersih)
+        except ValueError:
+            print("❌ Masukkan angka yang benar! Contoh: 100.000")
+
