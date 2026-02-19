bf8de47d25e938774308513bea4785d865870fce
danendra <dzaki.danen@gmail.com>
2026-02-19 14:15:57 +0700

    membuat bagian pelaporan program

--- saldo.py
+++ saldo.py
@@ -35,10 +35,27 @@ def input_kas(jenis):
     print("\n✅ Transaksi berhasil dicatat")
     print(f"Saldo saat ini : {format_rupiah(hitung_saldo())}")
     
-def input_kas(jenis):
+def laporan():
     conn = get_connection()
     cur = conn.cursor()
 
-    petugas = get_petugas()
-    keterangan = input("Keterangan      : ")
-    jumlah = input_rupiah("Jumlah (Rp)     : ")
+    cur.execute("SELECT tanggal, jenis, jumlah, keterangan, petugas FROM kas")
+    rows = cur.fetchall()
+    conn.close()
+
+    print("\n========== LAPORAN KAS TAKMIR ==========")
+
+    if not rows:
+        print("Belum ada transaksi.")
+        return
+
+    for t in rows:
+        print(
+            f"{t[0]} | "
+            f"{t[1].upper():6} | "
+            f"{format_rupiah(t[2]):>15} | "
+            f"{t[3]} | {t[4]}"
+        )
+
+    print("---------------------------------------")
+    print(f"Saldo Akhir : {format_rupiah(hitung_saldo())}")
